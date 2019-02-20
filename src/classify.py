9c13243f3f60a0f360047832070d41570bf605d5
David Talan <david.talan2@mail.dcu.ie>
2019-02-20 22:04:48 +0000

    classification model

--- src/classify.py
+++ src/classify.py
@@ -6,16 +6,23 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
 
 def detect():
-    df = pd.read_csv("/home/david/2019-ca400-taland2/src/train.csv")
-    df.shape
-    print(df.head())
-    """y = df.label
-    df.drop("label", axis1 = 1)
-    X_train, X_test , Y_train, Y_test = train_test_split(df['text'],y, test_size = 0.33, random_state = 53)
+    train_data = pd.read_csv('/home/david/2019-ca400-taland2/src/dataset/train.csv')
+    train_data.shape
+    print(train_data.head(3))
+
+    test_data = pd.read_csv('/home/david/2019-ca400-taland2/src/dataset/test.csv')
 
     vectorizer_count = CountVectorizer(stop_words = 'english')
-    train_count = vectorizer_count.fit_transform(X_train)
+    train_count = vectorizer_count.fit_transform(train_data)
+    test_count = vectorizer_count.transform(test_data)
 
-    test_count = vectorizer_count.transform(X_test)
-"""
+    tfidf_vectorizer = TfidVectorizer(stop_words = 'english', max_df = 0.7)
+    tfidf_train = tfidf_vectorizer.fit_transform(train_data)
+    tfidf_test = tfidf_vectorizer.transform(test_data)
+
+    print(tfidf_vectorizer.get_feature_name)
     #print(df.head())
+def main():
+    detect()
+
+if __name__ == "__main__": main()
