d9faf8c7ab277233cf8fd9953aa9a94ce6c622ef
David Talan <david.talan2@mail.dcu.ie>
2019-03-06 01:58:05 +0000

    Using new extractor

--- src/extract.py
+++ src/extract.py
@@ -1,4 +1,6 @@
-from boilerpipe.extract import Extractor
-
-extractor = Extractor(extractor='ArticleExtractor', url="https://www.bbc.com/news/world-africa-47089304")
-print (extractor.getText())
+from newspaper import Article
+url = 'https://www.rte.ie/news/business/2019/0305/1034553-brexit-construction/'
+article = Article(url)
+article.download()
+article.parse()
+print(article.text)
