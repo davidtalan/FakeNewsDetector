f36129ee3122deacb22d7bcd8017b5f8ea13f328
David Talan <david.talan2@mail.dcu.ie>
2019-03-08 00:33:50 +0000

    Working extractor

--- src/app.py
+++ src/app.py
@@ -1,12 +1,22 @@
 from flask import Flask,render_template,url_for,request
 from flask_bootstrap import Bootstrap
+from newspaper import Article
 
 app = Flask (__name__)
 Bootstrap(app)
-
+#//:TODO: Display extracted article
+#//:TODO:
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/handle_data', methods = ['POST'])
+def handle_data():
+    url = (request.form['article_link'])
+    article = Article(url)
+    article.download()
+    article.parse()
+    return render_template('/result.html', variable = article.text)
 
 @app.route('/result')
 def result():
